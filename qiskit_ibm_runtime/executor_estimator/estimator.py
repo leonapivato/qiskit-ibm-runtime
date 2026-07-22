5595b6c319b461d2518d08c16854d6c863732a63
Leonardo Pivato <105382480+leonapivato@users.noreply.github.com>
2026-07-22 16:06:28 -0400

    Apply suggestion from @leonapivato

--- qiskit_ibm_runtime/executor_estimator/estimator.py
+++ qiskit_ibm_runtime/executor_estimator/estimator.py
@@ -191,7 +191,7 @@ class EstimatorV2(BaseEstimatorV2):
 
             results = NoiseLearnerV3(mode).run(layers).result()
             
-            # This can be use to set the ``noise_model`` field of to ``est.options.resilience``
+            # This can be used to set the ``noise_model`` field of to ``est.options.resilience``
             # (please reword as you see fit, and check the correct place where this should end up in the options)
             noise_model = results.to_dict(layers)
 
